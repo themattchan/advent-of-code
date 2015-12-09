54669d52b2f9a4c64ac8aa61a100de5a36a9c081
Matthew Chan <matt@themattchan.com>
2015-12-09 01:47:16 -0800

    day1 part 2

--- day1/Main.hs
+++ day1/Main.hs
@@ -1,8 +1,12 @@
-main :: IO ()
+step f '(' = f + 1
+step f ')' = f - 1
+
+firstNeg = foldl go (0, 0)
+  where go (f,s) c = if f > -1 then (step f c, s+1) else (f,s)
+
 main = do
   ps <- readFile "input.txt"
-  let floor = foldl step 0 ps
-  print floor
-  where
-    step f '(' = f + 1
-    step f ')' = f - 1
+  let f = foldl step 0 ps
+      (_,s) = firstNeg ps
+  print f
+  print s
