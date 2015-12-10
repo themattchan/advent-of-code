2222a7c86fcdfd93ba2389a735510fcc30335d15
Matthew Chan <matt@themattchan.com>
2015-12-09 23:03:22 -0800

    remove gunk

--- day6/LightGrid.scala
+++ day6/LightGrid.scala
@@ -8,7 +8,6 @@ object LightGrid {
   case class Off    (tl: Coord, br: Coord) extends Instr
   case class Toggle (tl: Coord, br: Coord) extends Instr
 
-
   object ParseInstrs extends JavaTokenParsers {
 
     override def skipWhitespace = true
@@ -53,44 +52,3 @@ object LightGrid {
   }
 
 }
-
-/*
- object Wrap extends Dim {
-
- def surface(x: List[Int]) = {
-    val y = x ++ x
-    val b = y.zip(y.tail).take(x.length)
-    val c = b map { case (x,y) => x*y } reduce (_+_)
-    c*2
-  }
-
-  def slack(x: List[Int]) = x match {
-    case a::b::_ => a*b
-    case _ => throw new Exception
-  }
-
-  def wrapping(x: List[Int]) = surface(x) + slack(x)
-
-  def volume(x: List[Int]) = x.reduce(_*_)
-
-  def peri(x: List[Int]) = x match {
-    case a::b::_ => (a+b)*2
-    case _ => throw new Exception
-  }
-
-  def ribbon(x: List[Int]) = volume(x) + peri(x)
-
-  def main(args: Array[String]) = {
-    val input = new FileReader("input.txt")
-    val dims = parseAll(dim, input).getOrElse(List())
-
-    val totWrap = dims.map(wrapping).reduce(_+_)
-    println(totWrap)
-
-    val totRibbon = dims.map(ribbon).reduce(_+_)
-    println(totRibbon)
-
-    input.close
-  }
-}
- */
