25005ac9bc1151ed40d61ea190bb92ed8e86d36e
Matthew Chan <matt@themattchan.com>
2016-12-16 01:29:44 +0800

    day2

---
+++ 2016/day2/day2.hs
@@ -0,0 +1,15 @@
+import Data.List
+
+move k 'U' | k > 3          = k-3
+move k 'D' | k < 7          = k+3
+move k 'L' | k `mod` 3 /= 1 = k-1
+move k 'R' | k `mod` 3 /= 0 = k+1
+move k _ = k
+
+-- 1985
+test = "ULL\nRRDDD\nLURDL\nUUUUD"
+
+main = go <$> file >>= print
+  where
+    file = readFile "input.txt"
+    go = tail . scanl (foldl move) 5 . lines
