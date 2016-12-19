0b3baae0fbebffb294b5fa90512d612beab8d30a
Matthew Chan <matt@themattchan.com>
2016-12-19 20:26:03 +0800

    day5 part2

--- 2016/day5/day5.hs
+++ 2016/day5/day5.hs
@@ -1,7 +1,9 @@
 import Control.Arrow ((&&&))
+import Data.Function (on)
 import Data.List
-import Data.Ord
-
+import Data.Ord (comparing)
+import qualified Data.Map as M
+import qualified Data.Set as S
 import Data.Hash.MD5
 import Data.Word
 import Data.Monoid
@@ -14,13 +16,6 @@ abcdA (ABCD (a,_,_,_)) = a
 printHex :: Word32 -> String
 printHex n = printf "%x" n
 
--- swap32 :: Word32 -> Word32
--- swap32 w =   (w `shr` 24)            .|.  (w `shl` 24)
---         .|. ((w `shr` 8) .&. 0xff00) .|. ((w .&. 0xff00) `shl` 8)
---   where
---     shl = unsafeShiftL
---     shr = unsafeShiftR
-
 zeroPrefix :: Word32 -> Bool
 zeroPrefix x = (x .&. 0x00f0ffff) == 0
 
@@ -29,11 +24,6 @@ sixthHex x = (x `shift` (-16)) .&. 0xf
 
 seventhHex :: Word32 -> Word32
 seventhHex x = (x `shift` (-28)) .&. 0xf
-
--- foo
---   = map (printHex . sixthHex . swap32)
---   . map (abcdA . md5)
---   $ [Str "abc3231929"]
 
 doorId :: String
 doorId = "cxdnnyjw"
@@ -46,18 +36,18 @@ password1 doorId
   . map (abcdA . md5 . Str . (doorId <>) . show)
   $ [0..]
 
-
-validPosition x = sixthHex x >= 0 && sixthHex x <= 7
-
--- FIXME: no duplicates!!!!
--- password2 "abc"
--- [(1,5),(4,14),(7,3),(3,12),(0,0),(6,14),(7,11),(6,6)]
---password2 :: String -> String
+password2 :: String -> String
 password2 doorId
-  -- = foldMap (printHex . snd)
-  -- . sortBy (comparing fst)
-  = map (sixthHex &&& seventhHex)
-  . take 8
-  . filter (\x -> zeroPrefix x && validPosition x)
-  . map (abcdA . md5 . Str . (doorId <>) . show)
-  $ [0..]
+  = foldMap printHex . mconcat $ unfoldr go (0, S.fromList [0..7])
+  where
+    go (i,r)
+      | S.null r = Nothing
+      | otherwise = Just (h, (i+1, r'))
+      where
+        hash   = (abcdA . md5 . Str . (doorId <>)) (show i)
+        x      = sixthHex hash
+        y      = seventhHex hash
+        (h,r') | zeroPrefix hash && x `S.member` r
+               = (M.singleton x y, x `S.delete` r)
+               | otherwise
+               = (M.empty, r)
