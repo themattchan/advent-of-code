f2bc09a50104157744f30d4ed89215abb31fa3a4
Matthew Chan <matt@themattchan.com>
2016-12-19 20:49:34 +0800

    foo

--- 2016/day2/day2.hs
+++ 2016/day2/day2.hs
@@ -1,3 +1,5 @@
+import Control.Arrow ((***), (&&&))
+import Data.Bifunctor
 import Data.List
 
 move k 'U' | k > 3          = k-3
@@ -9,7 +11,14 @@ move k _ = k
 -- 1985
 test = "ULL\nRRDDD\nLURDL\nUUUUD"
 
-main = go <$> file >>= putStrLn . foldMap show
+move2 k 'U' | k > 3          = k-3
+move2 k 'D' | k < 7          = k+3
+move2 k 'L' | k `mod` 3 /= 1 = k-1
+move2 k 'R' | k `mod` 3 /= 0 = k+1
+move2 k _ = k
+
+main = go <$> file >>= print . bimap n n
   where
     file = readFile "input.txt"
-    go = tail . scanl (foldl move) 5 . lines
+    go = unzip . tail . scanl (foldl ((uncurry (&&&)) . (move *** move2))) (5,5) . lines
+    n = foldMap show
