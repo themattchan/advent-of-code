bdef22dd66691df2f60dc550e5b7bc68cf642084
Matthew Chan <matt@themattchan.com>
2016-12-19 21:12:26 +0800

    w combinator

--- 2016/day6/day6.hs
+++ 2016/day6/day6.hs
@@ -4,9 +4,7 @@ import Data.List
 import Data.Ord
 
 solve = (bimap `w` map head) . unzip . map (f (comparing length) . group . sort) . transpose
-  where
-    f = uncurry (&&&) . (maximumBy &&& minimumBy)
-    w f x = f x x
+  where f = uncurry (&&&) . (maximumBy &&& minimumBy)
+        w f x = f x x
 
-main = do
-  lines <$> readFile "input.txt" >>= print . solve
+main = lines <$> readFile "input.txt" >>= print . solve
