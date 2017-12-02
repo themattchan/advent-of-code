93944c93a9f5d3ee4064601e52d3e351f5820523
Matthew Chan <matt@awakenetworks.com>
2017-12-01 19:47:06 -0800

    refactor

--- 2017/day1/day1.hs
+++ 2017/day1/day1.hs
@@ -1,25 +1,15 @@
 #! /usr/bin/env runhaskell -i../../
 import Utils
 
-solve1 :: [Int] -> Int
-solve1 xs = getSum
-          . foldMap (Sum . fst)
-          . filter (uncurry (==))
-          . take (length xs)
-          $ zip (cycle xs) (tail (cycle xs))
-
-solve2 :: [Int] -> Int
-solve2 xs = getSum
-          . foldMap (Sum . fst)
-          . filter (uncurry (==))
-          . take len
-          $ zip (cycle xs) (drop n (cycle xs))
-  where
-    len = length xs
-    n = len `div` 2
+solve :: Int -> [Int] -> Int
+solve n = getSum
+        . foldMap (Sum . fst)
+        . filter (uncurry (==))
+        . uncurry zip
+        . (id &&& drop n . cycle)
 
 main :: IO ()
 main = do
   ints <- map digitToInt . filter isDigit <$> getContents
-  print $ solve1 ints
-  print $ solve2 ints
+  print $ solve 1 ints
+  print $ solve (length ints `div` 2) ints
