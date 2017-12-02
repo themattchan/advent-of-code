a8fa6aa905366feca58630b1893729ae401a16e5
Matthew Chan <matt@awakenetworks.com>
2017-12-01 19:31:34 -0800

    actually move utils

--- 2017/day1/day1.hs
+++ 2017/day1/day1.hs
@@ -4,10 +4,16 @@ import Utils
 
 solve1 :: [Int] -> Int
 solve1 xs = getSum
-      . foldMap (Sum . fst)
-      . filter (uncurry (==))
-      . take (length xs)
-      $ zip (cycle xs) (tail (cycle xs))
+          . foldMap (Sum . fst)
+          . filter (uncurry (==))
+          . take (length xs)
+          $ zip (cycle xs) (tail (cycle xs))
+
+solve2 :: [Int] -> Int
+solve2 xs = undefined
 
 main :: IO ()
-main = getContents >>= print . solve1 . map digitToInt . filter isDigit
+main = do
+  ints <- map digitToInt . filter isDigit <$> getContents
+  print $ solve1 ints
+  print $ solve2 ints
