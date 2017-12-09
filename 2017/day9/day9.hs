dc8d9cc600e373b2ad306d18221c2bf06d2411fc
Matthew Chan <matt@themattchan.com>
2017-12-08 21:40:38 -0800

    trivial

---
+++ 2017/day9/day9.hs
@@ -0,0 +1,21 @@
+#! /usr/bin/env runhaskell -i../../
+{-# LANGUAGE LambdaCase, BangPatterns #-}
+import Utils
+
+solve :: String -> (Int, Int)
+solve = go 0 0 0 . rmbang
+  where
+    go !l !s !g = \case
+      []     -> (s,g)
+      '{':xs -> go (l+1) s g xs
+      '}':xs -> go (l-1) (s+l) g xs
+      '<':xs -> let (gs,_:rest) = span (/='>') xs
+                in go l s (g+length gs) rest
+      _ : xs -> go l s g xs
+
+    rmbang [] = []
+    rmbang ('!' : _ : xs) = rmbang xs
+    rmbang (x:xs) = x : rmbang xs
+
+main :: IO ()
+main = readFile "input.txt" >>= print . solve
