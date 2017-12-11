bb4306521b7d8a41918f235c164c571062b72cb2
Matthew Chan <matt@awakenetworks.com>
2017-12-10 20:09:11 -0800

    tie the knot

--- 2015/day7/day7.hs
+++ 2015/day7/day7.hs
@@ -1,70 +1,39 @@
 #! /usr/bin/env runhaskell -i../../
-{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE LambdaCase, DeriveFunctor #-}
 import Utils
 import Data.Functor.Compose
 import Data.Bits
-import Control.Monad.Trans.State.Lazy
 import qualified Data.Map as M
-import qualified Data.Set as S
 
-type End = String
 type Input = Either String Int
-type Output = String
-data Gate = Pure Input
-          | Unop (Int -> Int) Input
-          | Binop (Int -> Int -> Int) Input Input
-data Wire = Wire Gate Output
 
-type Circuit a = State (M.Map String Int) a
+data Gate i = Pure i
+            | Unop (Int -> Int) i
+            | Binop (Int -> Int -> Int) i i
+            deriving Functor
 
-getVal :: String -> Circuit Int
-getVal v = gets (fromMaybe (error v) . M.lookup v)
+type Circuit = M.Map String (Gate Input)
+type Result = M.Map String Int
 
-runCircuit :: [Wire] -> Circuit ()
-runCircuit = foldM (const go) ()
+runCircuit :: Circuit -> Result
+runCircuit c = r
   where
-    getIn (Left v) = gets (fromJust . (M.lookup v))
-    getIn (Right n) = pure n
+    resolve (Left v)  = r M.! v
+    resolve (Right n) = n
 
-    go (Wire (Pure x) o) = getIn x >>= modify . M.insert o
-    go (Wire (Unop op x) o) = getIn x >>= modify . M.insert o . op
-    go (Wire (Binop op x y) o) = op <$> getIn x <*> getIn y >>= modify . M.insert o
+    evalGate :: Gate Int -> Int
+    evalGate (Pure x)       = x
+    evalGate (Unop op x)    = op x
+    evalGate (Binop op x y) = op x y
 
-toposort :: [Wire] -> [Wire]
-toposort wires = go starts rest
-  where
-    (starts, rest) = partition
-      (\case
-          (Wire (Pure (Right _)) _) -> True
-          _ -> False
-      ) wires
+    r = fmap (evalGate . fmap resolve) c
 
-    inscope = foldMap (\(Wire _ out) -> [out])
-
-    dependson (Wire (Pure x) _ ) = lefts [x]
-    dependson (Wire (Unop _ x) _ ) = lefts [x]
-    dependson (Wire (Binop _ x y) _ ) = lefts [x,y]
-
-    -- can produce output if all the inputs are in scope.
-    canproduce depends inscope =
-      depends `intersect` inscope == depends
-
-    go sorted [] = sorted
-    go sorted unsorted =
-      let
-        inscopes = inscope sorted
-        (sorted1, unsorted1) = partition
-          (\wire -> canproduce (dependson wire) inscopes)
-            unsorted
-      in
-        go (sorted ++ sorted1) unsorted1
-
-parseLine :: Parser Wire
+parseLine :: Parser (String, Gate Input)
 parseLine = do
     gate <- pBinop <|> pUnop <|> pLit
     string " -> "
     out <- pVar
-    return $ Wire gate out
+    return (out, gate)
   where
     pVar = alpha
     pInput = (Right <$> num) <|> (Left <$>pVar)
@@ -75,7 +44,7 @@ parseLine = do
        <|> literal "LSHIFT"  shiftL
        <|> literal "RSHIFT"  shiftR
 
-    pBinop, pUnop, pLit :: Parser Gate
+    pBinop, pUnop, pLit :: Parser (Gate Input)
     pBinop = do
       v1 <- pInput
       spaces
@@ -92,14 +61,16 @@ parseLine = do
 
     pLit = Pure <$> pInput
 
-solve1 :: [String] -> Int
-solve1 = flip evalState mempty
-       . (>> getVal "a")
-       . runCircuit
-       . toposort
-       . mapMaybe (runParser parseLine)
+solve :: [String] -> (Int,Int)
+solve ss = (part1, part2)
+  where
+    circuit = M.fromList
+            . mapMaybe (runParser parseLine)
+            $ ss
+    go c = runCircuit c M.! "a"
+    part1 = go circuit
+    part2 = go (M.insert "b" (Pure (Right part1)) circuit)
+
 
 main :: IO  ()
-main = do
-  instrs <- lines <$> readFile "input.txt"
-  print $ solve1 instrs
+main = readFile "input.txt" >>= print . solve . lines
