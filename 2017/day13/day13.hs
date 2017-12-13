b38de12a2269fe030e4385e119d9de71224293a7
Matthew Chan <matt@themattchan.com>
2017-12-13 02:00:06 -0800

    WIP

--- 2017/day13/day13.hs
+++ 2017/day13/day13.hs
@@ -83,21 +83,56 @@ main = do
 --------------------------------------------------------------------------------
 
 {-
--- there is an analytic solution to part 2!!!
--- some number theoretic thing to do with common factors...
--- level : depth
+How to number the levels:
 
+example: when depth = 3, the counter goes:
 
-Group: for depth n, define
-G_n = {0..2n-1}. this is the same as Z_2n-1 with the additional constraint that
-elements be interpreted in congruence class defined by (e - n).
+[x] -> [ ] -> [ ] -> [ ] -> [x] ....
+[ ]    [x]    [ ]    [x]    [ ] ....
+[ ]    [ ]    [x]    [ ]    [ ] ....
 
-in other words, the a scanner of depth d is characterised by the group
+ 0      1      2      3      4
 
-  S_d = Z_{2n-1} / phi
+this is Z_4. (0 == 4)
+
+0,2 are self inverses.
+1,3 are inverses.
+
+example: when depth = 4, the counter goes:
+
+[x] -> [ ] -> [ ] -> [ ] -> [ ] -> [ ] -> [x] ....
+[ ]    [x]    [ ]    [ ]    [ ]    [x]    [ ] ....
+[ ]    [ ]    [x]    [ ]    [x]    [ ]    [ ] ....
+[ ]    [ ]    [ ]    [x]    [ ]    [ ]    [ ] ....
+
+(0      1      2      3)     4      5      6
+
+see that endpoints are only hit once, while the middle bits are
+notice that 6 = 0, so this is Z_6 if you count the sequence.
+
+0,3 are self units
+2,4 are inverses
+1,5 are inverses.
+
+The counter is always in a group with two self inverses.
+
+the depth is Z_n. counter is Z_{ 2n - 2 }.
+
+in other words, the a scanner of depth n is characterised by the group
+
+  S_d = Z_{2n-2} / phi
     where
-      phi : Z_{2n-1} -> Z_n
-      phi(e) = (e - n)_n.
+      phi : Z_{2n-2} -> Z_n
+      phi(e) = if e > n then e^-1 else e
+
+it is obvious that the quotient group Z_{2n-2} is isomorphic to Z_n:
+when e = 0, e^-1 = 0
+when e = n-1, e^-1 = n-1 (where 2*(n-1) = 2n-2 = 0)
+otherwise, e = x, e^-1 = 2n-2-x. (pairs)
+
+furthermore, Z_n is a normal subgroup of Z_{2n-2}, because Z_{2n-2} is abelian
+and all subgroups of abelian groups are normal.
+
 
 
 levels: the number of "shifts" to 0 we need to add.
