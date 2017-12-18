d7bab1cfe5bcdf03d33465d0dfcf3a1e79e6c6a0
Matthew Chan <matt@awakenetworks.com>
2017-12-17 18:07:27 -0800

    solve

--- 2017/day17/day17.hs
+++ 2017/day17/day17.hs
@@ -8,15 +8,25 @@ import Utils
 
 solve n = head . tail . head . drop 2017 $ unfoldr go (1, [0]) where
   go (i, buf) = Just (buf, (i+1, buf'))
-    where buf' = (i :)  . take i . drop (n+1) . cycle $ buf
+    where buf' = (i :) . take i . drop (n+1) . cycle $ buf
 
---solve2 = head . tail . dropWhile (/= 0) . cycle . head . drop 50000000 . solve
+-- let input be n
+-- at iteration i: you will increase cursor by n mod i
+-- new cursor = (prev + n) mod i
+-- write to: (prev + n) mod i + 1
+-- can never overwrite 0? is 0 always at index 0? proof?
+insertInds n = scanl' go 0 [1..]
+  where
+    go prev i = (prev + n) `mod` i + 1
 
---solve2 n =  [1..]
+solve2 = last          -- most recent addition
+       . elemIndices 1 -- all i's where you write to index 1
+       . take 50000000
+       . insertInds
 
 main :: IO ()
 main = do
   n <- read <$> readFile "input.txt"
   print $ solve 3
   print $ solve n
---  print $ solve2 n
+  print $ solve2 n
