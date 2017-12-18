4e80c63367d30257ad32720ecc4eae3039635945
Matthew Chan <matt@awakenetworks.com>
2017-12-18 09:55:29 -0800

    nits

--- 2016/day7/day7.hs
+++ 2016/day7/day7.hs
@@ -11,19 +11,18 @@ import Debug.Trace
 solve :: [String] -> (Int, Int)
 solve = (part1 &&& part2) . map ((evens &&& odds) . splitOneOf "[]")
   where
-    part1 = length . filter findABBA
-    findABBA = uncurry (&&) . (hasABBA *** not . hasABBA)
-    hasABBA = or . map (any (\[w,x,y,z] -> w == z && x == y && w /= x) . window 4)
+    part1 = length . filter findTLS
+    findTLS = uncurry (&&) . (findABBA *** not . findABBA)
+    findABBA = or . map (any (\[w,x,y,z] -> w == z && x == y && w /= x) . window 4)
 
     part2 = length . filter findSSL
     findSSL = not . null . uncurry (intersectBy cmp) . (foldMap findABA *** foldMap findABA)
-
     findABA = filter (\[x,y,z] -> x == z && x /= y) . window 3
     cmp x [b,a,_] = x == [a,b,a]
 
-    evens = select (cycle [True,False])
-    odds  = select (cycle [False, True])
-    select = map snd . filter fst ... zip
+    evens = select [pure, const []]
+    odds  = select [const [], pure]
+    select = concat ... zipWith ($) . cycle
 
 main :: IO ()
 main = readFile "input.txt" >>= print . solve . lines
