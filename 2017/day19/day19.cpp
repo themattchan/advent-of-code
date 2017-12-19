73980ba4b758e96468e63d01345efb2025133e35
Matthew Chan <matt@awakenetworks.com>
2017-12-18 23:32:21 -0800

    restructure control flow

--- 2017/day19/day19.cpp
+++ 2017/day19/day19.cpp
@@ -6,19 +6,19 @@
 #include <algorithm>
 #include <iterator>
 #include <string>
-#include <locale>
+#include <ctype.h>
+
 using namespace std;
 
 #define dbcout if (! debugging) {} else cerr
 
-enum Dir { UP, DOWN, LEFT, RIGHT };
-
 #define notspace(c) (c != ' ')
+
+enum Dir { UP, DOWN, LEFT, RIGHT };
 
 int main ()
 {
   bool debugging = false;
-  locale LOCALE; // for isalpha
 
   ifstream input("input.txt");
   vector<string> MAP;
@@ -28,49 +28,30 @@ int main ()
   }
   int HEIGHT = MAP.size();
   int WIDTH = MAP[0].size();
-  dbcout << "read input, size is " << HEIGHT << " x " << WIDTH << endl;
 
-  for (auto s : MAP) {
-    dbcout << s << endl;
-  }
   // x coordinate (horizontal)
   int x = distance(MAP[0].begin(), find_if_not(MAP[0].begin(), MAP[0].end(), [](char c) {return c == ' ';}));
-  dbcout << "beginning is " << x << endl;
 
   // y coordinate (vertical)
-  int y = -1;
+  int y = 0;
   Dir d = DOWN;
-  string SEEN;
+  string seen;
   int steps = 0;
 
   while (1) {
-    steps++;
-
-    switch(d) {
-    case UP: y--; break;
-    case DOWN: y++; break;
-    case LEFT: x--; break;
-    case RIGHT: x++; break;
-    }
-
     if (x < 0 || x >= WIDTH || y < 0 || y >= HEIGHT) {
-      dbcout << "DONE " << "x=" <<x << " y=" <<y<<endl;
       break;
     }
-    dbcout << "x=" <<x << " y=" <<y<<endl;
 
     char c = MAP[y][x];
-    dbcout << "c is "  <<c << endl;
 
-    if (isalpha(c, LOCALE)) {
-      SEEN += c;
-
-      dbcout << "SEEN "  <<c << endl;
-
-      // hack so it doesn't count the last space after 'Z'
-      if (c == 'Z') break;
+    // scrutinise this square
+    if (isalpha(c)) {
+      seen += c;
     }
-
+    else if (c == ' ') {
+      break;
+    }
     else if (c == '+') {
       switch(d) {
       case UP:
@@ -91,9 +72,21 @@ int main ()
       }
       }
     }
+
+    // finished step
+    steps++;
+
+    // take next step
+    switch(d) {
+    case UP: y--; break;
+    case DOWN: y++; break;
+    case LEFT: x--; break;
+    case RIGHT: x++; break;
+    }
+
   }
 
-  cout << "part 1 " << SEEN << endl;
+  cout << "part 1 " << seen << endl;
   cout << "part 2 " << steps << endl;
   return 0;
 }
