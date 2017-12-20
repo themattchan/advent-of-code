2d71467f4f170a091297a0f2cc99534d8be044c6
Matthew Chan <matt@awakenetworks.com>
2017-12-19 22:44:52 -0800

    cleanup

--- 2017/day20/day20.hs
+++ 2017/day20/day20.hs
@@ -1,43 +1,40 @@
 #! /usr/bin/env runhaskell -i../../
 {-# LANGUAGE BangPatterns  #-}
-{-# LANGUAGE DeriveFunctor, DeriveFoldable, DeriveTraversable #-}
+{-# LANGUAGE DeriveFunctor #-}
 {-# LANGUAGE LambdaCase    #-}
 {-# LANGUAGE PatternGuards #-}
 {-# LANGUAGE TupleSections #-}
 {-# LANGUAGE RecordWildCards #-}
 import Utils
-import qualified Data.Vector.Unboxed as V
 import qualified Data.Semigroup as S
 import Data.Int
 
-newtype V3 a = V3 [a] --(V.Vector a) --deriving (Functor, Foldable, Traversable)
-  deriving (Show, Eq)
-mkV3 :: V.Unbox a => [a] -> V3 a
-mkV3 xs = V3 xs -- $ V.fromList xs
+newtype V3 a = V3 [a] deriving (Show, Eq)
 
-instance (Num a, V.Unbox a) => S.Semigroup (V3 a) where
+instance (Num a) => S.Semigroup (V3 a) where
   V3 xs <> V3 ys = V3 (zipWith (+) xs ys)
   {-# INLINEABLE (<>) #-}
   {-# SPECIALISE (<>) :: V3 Int64 -> V3 Int64 -> V3 Int64 #-}
 
-distV3 :: (Num a, V.Unbox a)=> V3 a -> a
+distV3 :: (Num a)=> V3 a -> a
 distV3 (V3 v) = sum $ map abs v
+{-# INLINEABLE distV3 #-}
 {-# SPECIALISE distV3 :: V3 Int64 -> Int64 #-}
 
-data Particle = Particle { particleP :: V3 Int
-                         , particleV :: V3 Int
-                         , particleA :: V3 Int
-                         } deriving Show
+data Particle = Particle
+  { particleP :: V3 Int64
+  , particleV :: V3 Int64
+  , particleA :: V3 Int64
+  } deriving Show
 
 parser :: Parser [Particle]
 parser = many (particle <* spaces)
   where
-    particle = do
-      Particle
-      <$> (string "p=" *> pvec <* string ", ")
-      <*> (string "v=" *> pvec <* string ", ")
-      <*> (string "a=" *> pvec)
-    pvec = mkV3  <$> between (char '<') (char '>') (sepBy num (char ','))
+    particle =
+      Particle <$> (string "p=" *> vec <* string ", ")
+               <*> (string "v=" *> vec <* string ", ")
+               <*> (string "a=" *> vec)
+    vec = V3 <$> between (char '<') (char '>') (sepBy num (char ','))
 
 -- p0 = <x0,y0,z0>
 -- v = <vx,vy,vz>
@@ -59,14 +56,16 @@ minParticle = fst . minimumBy cmp . zip [0..]
   where
     accel = distV3 . particleA
     init = distV3 . particleP
-    cmp (_,p1) (_,p2) = case compare (accel p1) (accel p2) of
-      EQ -> compare (init p1) (init p2)
-      x -> x
+    cmp (_,p1) (_,p2) =
+      case compare (accel p1) (accel p2) of
+        EQ -> compare (init p1) (init p2)
+        x -> x
 
 stepParticle :: Particle -> Particle
-stepParticle p@Particle{..} = p { particleP = particleP S.<> particleV S.<> particleA
-                                , particleV = particleV S.<> particleA
-                                }
+stepParticle p@Particle{..}
+  = p { particleP = particleP S.<> particleV S.<> particleA
+      , particleV = particleV S.<> particleA
+      }
 
 collideUntilDone :: [Particle] -> Int
 collideUntilDone xs
@@ -74,17 +73,18 @@ collideUntilDone xs
   | otherwise = collideUntilDone xs'
   where
     colls = map head . filter ((>1) . length) . groupBy (==) . map particleP $ xs
+
     xs' = map stepParticle . filter (not . (`elem` colls) . particleP) $ xs
 
-    ordered cmp xs = all (`elem` [LT,EQ]) $ zipWith cmp xs (tail xs)
+    ordered cmp xs = all (/= GT) $ zipWith cmp xs (tail xs)
 
-    done = let sorted = sortOn (distV3 . particleP) xs
-           in ordered (comparing (distV3 . particleV)) sorted && ordered (comparing (distV3 . particleA)) sorted
-
+    done = ordered (comparing (distV3 . particleV)) sorted
+        && ordered (comparing (distV3 . particleA)) sorted
+      where
+        sorted = sortOn (distV3 . particleP) xs
 
 main :: IO ()
 main = do
   Just ps <- runParser parser <$> readFile "input.txt"
---  print ps
   print $ minParticle ps
   print $ collideUntilDone ps
