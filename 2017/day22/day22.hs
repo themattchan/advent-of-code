ff26ed384ca4e9a7dcaf13f9f96a24f2669990f7
Matthew Chan <matt@awakenetworks.com>
2017-12-21 22:50:13 -0800

    part 2

--- 2017/day22/day22.hs
+++ 2017/day22/day22.hs
@@ -6,12 +6,9 @@
 {-# LANGUAGE TupleSections #-}
 import Utils
 import qualified Data.Set as S
+import qualified Data.Map as M
 
 data Dir = U | R | D | L deriving (Show, Eq, Enum)
-
--- instance Enum Dir where
---   toEnum n = [Up,Down,Left,Right] !! n `mod` 4
---   fromEnum e = elemIndex e [Up,Down,Left,Right]
 
 turnLeft, turnRight :: Dir -> Dir
 turnLeft U = L
@@ -22,12 +19,16 @@ turnRight x = succ x
 
 data Coord = C {-#UNPACK#-}!Int {-#UNPACK#-}!Int deriving (Show,Ord, Eq)
 
-type State = (Coord, Dir, Grid, Int)
+type State grid = (Coord, Dir, grid, Int)
 
 getInfectMoveCount (_,_,_,i) = i
 
-move :: State -> State
-move (c@(C x y), d, g, infect) = (c', d', g', infect')
+type Grid = S.Set Coord
+
+readInput1 = S.fromList . readInput
+
+move1 :: State Grid -> State Grid
+move1 (c@(C x y), d, g, infect) = (c', d', g', infect')
   where
     isInfected = c `S.member` g
 
@@ -46,10 +47,54 @@ move (c@(C x y), d, g, infect) = (c', d', g', infect')
       D -> C x (y-1)
       R -> C (x+1) y
 
-type Grid = S.Set Coord
+evolve1 :: String -> [State Grid]
+evolve1 = iterate move1 . mkInitState . readInput1
+  where
+    mkInitState g = (C 0 0, U, g, 0)
 
-readInput :: String -> Grid
-readInput s = S.fromList [C x y | (y, xs) <- indexed, (x, c) <- xs, c == '#']
+--------------------------------------------------------------------------------
+
+data CoordSt = {- Clean (not in map) | -} Weakened | Infected | Flagged  deriving (Show, Ord, Eq, Enum)
+
+type Grid2 = M.Map Coord CoordSt
+
+readInput2 :: String -> Grid2
+readInput2 = M.fromList . flip zip (repeat Infected) . readInput
+
+move2 :: State Grid2 -> State Grid2
+move2 (c@(C x y), d, g, infect) = (c', d', g', infect')
+  where
+    cSt = c `M.lookup` g
+
+    d' = case cSt of
+      Nothing -> turnLeft d
+      Just Weakened -> d
+      Just Infected -> turnRight d
+      Just Flagged -> turnRight (turnRight d)
+
+    g' = case cSt of
+      Just Flagged -> M.delete c g -- clean
+      Nothing -> M.insert c Weakened g
+      _ -> M.adjust succ c g
+
+    infect' | cSt == Just Weakened = infect + 1
+            | otherwise = infect
+
+    c' = d' & \case
+      U -> C x (y+1)
+      L -> C (x-1) y
+      D -> C x (y-1)
+      R -> C (x+1) y
+
+evolve2 :: String -> [State Grid2]
+evolve2 = iterate move2 . mkInitState . readInput2
+  where
+    mkInitState g = (C 0 0, U, g, 0)
+
+--------------------------------------------------------------------------------
+
+readInput :: String -> [Coord]
+readInput s = [C x y | (y, xs) <- indexed, (x, c) <- xs, c == '#']
   where
     l = lines s
     h = length l
@@ -57,21 +102,21 @@ readInput s = S.fromList [C x y | (y, xs) <- indexed, (x, c) <- xs, c == '#']
     coords n = let n' = (n-1) `div` 2 in [-n'..n']
     indexed = zip (reverse (coords h)) . map (zip (coords w)) $ l
 
-
-evolve :: String -> [State]
-evolve = iterate move . mkInitState . readInput
-  where
-    mkInitState g = (C 0 0, U, g, 0)
-
-solve1 :: Int -> [State] -> Int
-solve1 n ss = getInfectMoveCount $ ss !! n
+solve :: Int -> [State g] -> Int
+solve n ss = getInfectMoveCount $ ss !! n
 
 test = "..#\n#..\n..."
 
 main :: IO ()
 main = do
-  print $ solve1 70 (evolve test)  == 41
-  print $ solve1 10000 (evolve test)  == 5587
   xs <- readFile "input.txt"
-  print $ solve1 10000 (evolve xs)
-  print $ solve1 10000000 (evolve xs)
+
+  putStrLn $ "PART 1"
+  print $ solve 70    (evolve1 test) == 41
+  print $ solve 10000 (evolve1 test) == 5587
+  print $ solve 10000 (evolve1 xs)
+
+  putStrLn $ "\nPART 2"
+  print $ solve 100      (evolve2 test) == 26
+  print $ solve 10000000 (evolve2 test) == 2511944
+  print $ solve 10000000 (evolve2 xs)
