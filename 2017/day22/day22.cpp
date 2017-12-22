7c1a4872e9adb831787697a33a1ad2c317d82aca
Matthew Chan <matt@awakenetworks.com>
2017-12-22 01:15:06 -0800

    maybe i should just have used c++...

---
+++ 2017/day22/day22.cpp
@@ -0,0 +1,143 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <map>
+#include <algorithm>
+#include <iterator>
+#include <string>
+#include <utility>
+#include <ctype.h>
+
+using namespace std;
+
+#define dbcout if (! debugging) {} else cerr
+
+enum Dir { UP, RIGHT, DOWN, LEFT };
+Dir operator++(Dir& d)
+{
+    d = static_cast<Dir>((d + 1) % 4);
+    return d;
+}
+Dir operator--(Dir& d)
+{
+    d = static_cast<Dir>((d +3) % 4);
+    return d;
+}
+
+enum State { Clean, Weakened, Infected, Flagged };
+State operator++(State& st)
+{
+  st = static_cast<State>((st + 1) % 4);
+  return st;
+}
+State operator--(State& st)
+{
+  st = static_cast<State>((st +3) % 4);
+  return st;
+}
+
+typedef map<pair<int,int>, State> Grid;
+
+void turnBySt(const State& st, Dir& d)
+{
+  switch (st) {
+  case Clean: --d; return;
+  case Weakened: return;
+  case Infected: ++d; return;
+  case Flagged: ++d; ++d; return;
+  }
+}
+
+void step(int& x, int& y, const Dir& d)
+{
+  switch (d) {
+  case UP: y++; break;
+  case LEFT: x--; break;
+  case DOWN: y--; break;
+  case RIGHT: x++; break;
+  }
+}
+
+int solve1 (Grid GRID, int N)
+{
+  int X = 0;
+  int Y = 0;
+  Dir d = UP;
+  int infect = 0;
+
+  while (N--) {
+    auto xy = make_pair(X,Y);
+    const State oldSt = GRID[xy];
+    if (oldSt == Clean) {
+      GRID[xy] = Infected;
+      infect++;
+    } else if (oldSt == Infected ) {
+      GRID.erase(xy);
+    }
+    turnBySt(oldSt, d);
+    step(X,Y,d);
+  }
+  return infect;
+}
+
+int solve2 (Grid GRID, int N)
+{
+  int X = 0;
+  int Y = 0;
+  Dir d = UP;
+  int infect = 0;
+
+  while (N--) {
+    auto xy = make_pair(X,Y);
+    const State oldSt = GRID[xy];
+
+    switch (oldSt) {
+    case Clean:
+      GRID[xy] = Weakened;
+      break;
+    case Flagged:
+      GRID.erase(xy);
+      break;
+    case Weakened:
+      infect++; // fallthrough
+    default:
+      ++GRID[xy];
+      break;
+    }
+
+    turnBySt(oldSt, d);
+    step(X,Y,d);
+  }
+  return infect;
+}
+
+int main (int argc, char *argv[])
+{
+  bool debugging = false;
+
+  Grid GRID;
+  {
+    string file(argv[1]);
+    ifstream input(file);
+
+    vector<string> lines;
+    std::copy(std::istream_iterator<string>(input),
+              std::istream_iterator<string>(),
+              std::back_inserter(lines));
+
+    int y = (lines.size() -1) /2;
+    for (auto l : lines) {
+      int x = -1 * ((l.size() -1)/2);
+      for (auto c : l) {
+        if (c == '#') {
+          GRID.insert(make_pair(make_pair(x,y), Infected));
+        }
+        x++;
+      }
+      y--;
+    }
+  }
+
+  cout << solve1(GRID, 10000) <<endl;
+  cout << solve2(GRID, 10000000) <<endl;
+}
