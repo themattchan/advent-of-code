3a95a47868a4576001392604e76a8224fe278d3c
Matthew Chan <matt@awakenetworks.com>
2018-11-11 02:27:23 -0800

    no bugs but this brute force search is super slow...

--- 2015/day11/day11.c
+++ 2015/day11/day11.c
@@ -7,10 +7,12 @@ bool check3conseq(char * s)
 	char *s0 = s+0;
 	char *s1 = s+1;
 	char *s2 = s+2;
-	while (s2) {
-		if (*s0+1 == *s1 && *s1+1 == *s2)
+	while (s2 && s0 && s1) {
+//		puts("HERE");
+		if ((*s0)+1 == *s1 && (*s1)+1 == *s2) {
+//			puts("YES");
 			return true;
-
+		}
 		s0++; s1++; s2++;
 	}
 	return false;
@@ -36,11 +38,11 @@ bool twooverlap(char *s)
 			x++;
 			s0 += 2; s1 += 2;
 		}
-
-		s0++; s1++;
+		else {
+			s0++; s1++;
+		}
 	}
 	return x >= 2;
-
 }
 
 #define INCR(x) ((x) == 'z' ? 'a' : (x)+1)
@@ -50,17 +52,25 @@ void incrs(char *sbeg, char *send)
 	char* cur = send;
 	*cur = INCR(*cur);
 	while (*cur == 'a' && cur != sbeg) {
-		cur=cur-1;
+		cur--;
 		*cur = INCR(*cur);
 	}
 }
 
-int main()
+int main(int argc, char * argv[])
 {
-	char * in = "hxbxwxba";
-	int end = strlen(in);
+//	char * in = "hxbxwxba";
+	if (argc < 2) {
+		puts("NO INPUT\n");
+		return 1;
+	}
+	char *in = argv[1];
+	int end = strlen(in) -1;
+//	printf("strlen: %d\n", end);
+//	printf("lastchar: %c\n", *(in+end));
 	while (! (check3conseq(in) && noiol(in) && twooverlap(in))) {
 		incrs(in, in+end);
+//		printf("INCREMENTED: %s\n",in);
 	}
 	puts(in);
 	return 0;
