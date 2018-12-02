6ef1b9279498dbf5472ecb82d7b87b5bcb99c31c
Matthew Chan <matt@themattchan.com>
2018-12-01 23:55:11 -0800

    day1 hs

--- 2018/day1/day1.hs
+++ 2018/day1/day1.hs
@@ -1,14 +1,12 @@
 {-# LANGUAGE TypeApplications #-}
-import Data.List
 import qualified Data.IntSet as IS
-import Debug.Trace
 main = do
   input <- map (read @Int . filter (/='+')) . lines <$> readFile "input"
-  let as = (scanl (+) 0 . cycle $ input)
-  print $ take 100 as
+
+  print (sum input)
+
   let go seen (x:xs) | x `IS.member` seen = x
                      | otherwise =  -- trace (show x) $
                                   go (x `IS.insert` seen) xs
 
-  print $ go mempty as
-  pure ()
+  print $ go mempty (scanl (+) 0 . cycle $ input)
