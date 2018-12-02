773caa748fa8679c2f6ba5bbc4d9912d96a3ee64
Matthew Chan <matt@themattchan.com>
2018-12-01 23:28:34 -0800

    part 2

--- 2018/day2/day2.hs
+++ 2018/day2/day2.hs
@@ -1,8 +1,17 @@
 import Control.Arrow
 import Data.List
 
-f n = not . null . filter (==n) . map length . group . sort
-s2 = length . filter (f 2)
-s3 = length . filter (f 3)
-solve1 = uncurry (*) . (s2 &&& s3)
-main = print . solve1 . lines =<< readFile "input.txt"
+solve1 :: [String] -> Int
+solve1 = (*) <$> s2 <*> s3
+  where
+    f n = not . null . filter (==n) . map length . group . sort
+    s2 = length . filter (f 2)
+    s3 = length . filter (f 3)
+
+solve2 :: [String] -> Maybe String
+solve2 = fmap (map fst . snd) . find ((==1) . length . fst) . map diff . (zip <*> tail) . sort
+  where
+    diff = (filter (uncurry (/=)) &&& filter (uncurry (==))) . uncurry zip
+
+main :: IO ()
+main = print . (solve1 &&& solve2) . lines =<< readFile "input.txt"
