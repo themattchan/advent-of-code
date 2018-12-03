aeae5cd901cd96ec4c9972df9df9542b7e0a7fa6
Matthew Chan <matt@themattchan.com>
2018-12-03 01:43:28 -0800

    hack the lookup function

--- 2018/day3/day3.hs
+++ 2018/day3/day3.hs
@@ -1,12 +1,16 @@
 {-# LANGUAGE ViewPatterns #-}
 
 import Utils
-import qualified Data.RTree as RTree
+import qualified Data.RTree as R
+import qualified Data.RTree.Base as R
+import qualified Data.RTree.MBB as R
+import qualified Data.IntSet as S
+import Debug.Trace
 
-mbbi :: Int -> Int -> Int -> Int -> RTree.MBB
-mbbi x y x' y' = RTree.mbb (fromIntegral x) (fromIntegral y) (fromIntegral x') (fromIntegral y')
+mbbi :: Int -> Int -> Int -> Int -> R.MBB
+mbbi x y x' y' = R.mbb (fromIntegral x) (fromIntegral y) (fromIntegral x') (fromIntegral y')
 
-parseRT :: Parser (RTree.RTree [Int])
+parseRT :: Parser (R.RTree [Int])
 parseRT = do
   char '#'
   i <- num
@@ -22,13 +26,42 @@ parseRT = do
   char 'x'
   h <- num
   spaces
-  return (RTree.singleton (mbbi x y (x+w) (y+h)) [i])
+  let m = (mbbi x y (x+w) (y+h))
+--  trace ("parsed mbb: "<>show m) $ pure ()
+  return (R.singleton m [i])
+
+readRT file = do
+  Just rt <- fmap mconcat . runParser (many parseRT) <$> readFile file
+  return rt
+
+-- | returns all keys and values, which contain the given point
+lookupPointWithKey :: R.MBB -> R.RTree a -> [(R.MBB, a)]
+lookupPointWithKey _ R.Empty = []
+lookupPointWithKey mbb t@R.Leaf{}
+    | (R.getMBB t) `R.containsMBB` mbb = [(R.getMBB t, R.getElem t)]
+    | otherwise = []
+lookupPointWithKey mbb t = founds
+    where
+    matches = filter intersectRTree $ R.getChildren t
+    founds = concatMap (lookupPointWithKey mbb) matches
+    intersectRTree x = isJust $ mbb `R.intersectMBB` (R.getMBB x)
+
+-- | returns all values, which are located in the given bounding box.
+lookupPoint :: R.MBB -> R.RTree a -> [a]
+lookupPoint mbb t = snd <$> (lookupPointWithKey mbb t)
+
 
 main :: IO ()
 main = do
-  Just rt <- fmap mconcat . runParser (many parseRT) <$> readFile "input"
-  print $ length [() | i <- [0..1000]
-                     , j <- [0..1000]
-                     , let ids = RTree.lookup (mbbi i j i j) rt
-                     , length ids >= 2
-                     ]
+  rt <- readRT "input"
+--  print rt
+--  print $ R.keys rt
+--  print $ R.values rt
+  let overlaps = [ids
+                 | i <- [0..1000]
+                 , j <- [0..1000]
+                 , let ids = lookupPoint (mbbi i j (i+1) (j+1)) rt
+                 , length ids >= 2
+                 ]
+  print (length overlaps)
+  print (foldMap S.fromList (R.values rt) `S.difference` foldMap (foldMap S.fromList) overlaps)
--- 2018/day3/day3.nix
+++ 2018/day3/day3.nix
@@ -1,10 +1,10 @@
-{ mkDerivation, base, myutils, data-r-tree, stdenv }:
+{ mkDerivation, base, myutils, data-r-tree, containers, stdenv }:
 mkDerivation {
   pname = "day3";
   version = "1.0.0";
   src = ./.;
   isLibrary = false;
   isExecutable = true;
-  executableHaskellDepends = [ base myutils data-r-tree ];
+  executableHaskellDepends = [ base myutils data-r-tree containers];
   license = stdenv.lib.licenses.bsd3;
 }
