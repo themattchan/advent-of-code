7e5f16f1ac1a9d30ac2f3a6adbeefefe2bf2eabc
Matthew Chan <matt@themattchan.com>
2018-12-03 23:59:32 -0800

    WHY THE FUCK IS THIS WRONG

--- 2018/day4/day4.py
+++ 2018/day4/day4.py
@@ -15,9 +15,9 @@ def parse(file):
             for l in input:
                 m = re.match('\[(\d{4})-(\d{2})-(\d{2}) (\d{2}):(\d{2})\] (wakes up|falls asleep|Guard #(\d+) begins shift)', l)
                 d = dt.datetime(int(m.group(1)), int(m.group(2)), int(m.group(3)), int(m.group(4)), int(m.group(5)))
-                evt = m.group(6)[0]
-                newGuard = m.group(7)
-                x = int(newGuard) if newGuard != None else evt
+                event = m.group(6)[0]
+                new_guard = m.group(7)
+                x = int(new_guard) if new_guard != None else event
                 yield (d,x)
     return sorted(go(file), key=fst)
 
@@ -36,17 +36,22 @@ def rangesize(r):
   (x,y)=r
   return y-x
 
+def count_in_ranges(rs, i):
+  return ft.reduce((lambda acc,r: acc + inrange(i,r)), rs, 0)
+
+def dt_ranges_to_min_ranges(rs): return list(map(lambda d: (d[0].minute, d[1].minute),rs))
+
 # given a set of ranges, for each min in the hour, count how many times that minute occs in the set
 # build_freqs_map :: list range[datetime] -> map int int
 def build_freqs_map(rs):
-  rs = list(map(lambda d: (d[0].minute, d[1].minute),rs))
+  rs = dt_ranges_to_min_ranges(rs)
   def gen():
-    for i in range(0,59):
-      x = ft.reduce((lambda acc,r: acc + inrange(i,r)), rs, 0)
-      yield (i, x)
+    for i in range(0,60):
+      yield (i, count_in_ranges(rs,i))
   return dict(gen())
 
 # build a map of all guards and their sleeping intervals
+# build_sleepmap :: events -> map guard (list range)
 def build_sleepmap(events):
   def go(l, r):
     (acc, cg) = l
@@ -67,20 +72,50 @@ def best_minute(freqs):
 
 ################################################################################
 
-events = parse('input.txt')
+events = parse('input.txt') # parse('test')
 
 sleepmap = build_sleepmap(events)
 sleepyGuard = max(sleepmap.keys(), key=lambda k: ft.reduce(lambda acc, r: acc+rangesize(r), sleepmap.get(k), dt.timedelta()))
-
+print ('sleepyGuard='+str(sleepyGuard))
 freqs = build_freqs_map(sleepmap[sleepyGuard])
 
 bestTime = best_minute(freqs)[0]
 
 ## part 1
-print (bestTime * sleepyGuard)
+print ('part 1='+str(bestTime * sleepyGuard))
 
 
+def mapValues(f, m):
+  return { k: f(v) for k, v in m.items() }
 
-# all_guards_best = { k: best_minute(build_freqs_map(v)) for k, v in sleepmap.items() }
-# maxSleepyGuard = max(all_guards_best.keys(), key=lambda k: all_guards_best.get(k)[1])
-# print (maxSleepyGuard * all_guards_best[maxSleepyGuard][0])
+def part2(sleepmap):
+    def go():
+      for i in range(0,60):
+        # for each guard, compute how many times he is asleep in minute i
+        x = mapValues(lambda rs: count_in_ranges(dt_ranges_to_min_ranges(rs), i), sleepmap)
+
+        # find the guard who sleeps the most in minute i
+        y = max(x.keys(), key=x.get)
+
+        # return him and the number of times he slept
+        ret = (i, (y, x[y]))
+        print(ret)
+
+        yield ret
+
+    # for all minutes, guard who sleeps the most.
+    best_guards_map = dict(go())
+
+    best_min = max(best_guards_map.keys(), key=lambda k: best_guards_map[k][1])
+    print(best_min)
+
+    best_guard = best_guards_map[best_min][0]
+    print(best_guard)
+
+    return (best_min * best_guard)
+
+events = parse('input.txt') # parse('test')
+
+sleepmap = build_sleepmap(events)
+
+print(part2(sleepmap))
