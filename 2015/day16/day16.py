13d864d3340139afa1c38cf96cd569b3690a98d1
Matthew Chan <matt@awakenetworks.com>
2018-12-05 12:47:43 -0800

    nits

--- 2015/day16/day16.py
+++ 2015/day16/day16.py
@@ -1,5 +1,4 @@
 import re
-import itertools as it
 
 def parse(file):
   def go():
@@ -7,7 +6,6 @@ def parse(file):
       for l in input:
         m = re.match('Sue (\d+): (\w+): (\d+), (\w+): (\d+), (\w+): (\d+)', l)
         n = int(m.group(1))
-#        x = { m.group(2): int(m.group(3)), m.group(4): int(m.group(5)), m.group(6): int(m.group(7)) }
         x = [ (m.group(2), int(m.group(3))), (m.group(4), int(m.group(5))), (m.group(6), int(m.group(7))) ]
         yield n, x
   return go()
@@ -17,12 +15,13 @@ gt_attrs = { 'cats': 7, 'trees': 3 }
 lt_attrs = { 'pomeranians': 3, 'goldfish': 5 }
 all_attrs = dict(gt_attrs, **lt_attrs, **eq_attrs)
 
-def solve(entries, eqs,lts={}, gts={}):
+def solve(entries, eqs, lts={}, gts={}):
   def f(x):
     i, xs = x
     return (   all([eqs[k] == v for k,v in xs if k in eqs])
            and all([lts[k] > v  for k,v in xs if k in lts])
-           and all([gts[k] < v  for k,v in xs if k in gts]))
+           and all([gts[k] < v  for k,v in xs if k in gts])
+           )
   return list(filter(f, entries))[0][0]
 
 print(solve(parse('input'), all_attrs))
