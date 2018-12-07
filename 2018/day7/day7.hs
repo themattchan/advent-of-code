c386d24db0924b72b586ffcde3742f57e99677cb
Matthew Chan <matt@themattchan.com>
2018-12-07 00:21:59 -0800

    fuck

--- 2018/day7/day7.hs
+++ 2018/day7/day7.hs
@@ -9,8 +9,8 @@ import Debug.Trace
 
 
 --t1 :: [String] -> String
-t1 is = map (\x -> fst $ fromJust $ find ((== x) . snd) vs)
-        (G.topSort graph)
+t1 is = trace ("vs: " <> show vs) $ map (\x -> fst $ fromJust $ find ((== x) . snd) vs)
+         (myTopSort vs es' graph)
 --        trace ("ggg: "<> show vs) $
 --          (reverse (G.topSort (G.transposeG graph)))
   where
@@ -28,7 +28,21 @@ t1 is = map (\x -> fst $ fromJust $ find ((== x) . snd) vs)
     getIt (Just (_,ss,_)) = edge $ foldMap ((:[]) . fst) ss
     getIt Nothing = error "regex failed"
 
-    edge [_,[x],[y]] = [(x, y)]
+    edge [_,[x],[y]] = trace ([x]<> " " <>[y]) [(x, y)]
     edge x = error ("edge: " <> show x)
 
-main = putStrLn . t1 . lines =<< readFile "test"
+--myTopSort :: [G.Edge] -> G.Graph -> [G.Vertex]
+myTopSort vs es g = go [] (go [] (G.vertices g))
+  where
+    vOuts done v = reverse $ sort [j | (i,j) <- es, i==v, j `notElem` done]
+    go out [] = out
+    go out (v:todo)
+--      | trace ("DO: "<> show v <> show (lookup v (map swap vs))) False = undefined
+      | null (vOuts out v) = go (v:out) todo
+      | otherwise = let doit = (\x -> trace ("doit " <> show x) x) $ vOuts out v
+                        todo' = todo \\ doit
+                    in go out (doit ++ (v:todo'))
+
+
+
+main = putStrLn . t1 . lines =<< readFile "input"
