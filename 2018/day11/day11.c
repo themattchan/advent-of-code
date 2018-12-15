5df4a4764dd54114a1f0e77143a36dcf696e0416
Matthew Chan <matt@themattchan.com>
2018-12-15 03:00:04 -0800

    nit

--- 2018/day11/day11.c
+++ 2018/day11/day11.c
@@ -30,7 +30,7 @@ int main ()
     for (int y = 1; y <= SIZE-2; ++y) {
       for (int x = 1; x <= SIZE-2; ++x) {
         int newMaxP =
-          grid[y  ][x] + grid[y  ][x+1] + grid[y  ][x+2]
+            grid[y  ][x] + grid[y  ][x+1] + grid[y  ][x+2]
           + grid[y+1][x] + grid[y+1][x+1] + grid[y+1][x+2]
           + grid[y+2][x] + grid[y+2][x+1] + grid[y+2][x+2];
 
