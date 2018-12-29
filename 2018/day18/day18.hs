2c1da2e07d3546bf787960a8def805240023098f
Matthew Chan <matt@themattchan.com>
2018-12-28 23:43:42 -0800

    cut out loop

--- 2018/day18/day18.hs
+++ 2018/day18/day18.hs
@@ -4,6 +4,7 @@ import qualified Data.Array.MArray as MA
 import qualified Data.Array.IO as MA
 import qualified Data.Array.Base as MA
 import qualified Data.Ix as Ix
+import qualified Data.Map as M
 import Utils
 
 --data X = Open | Tree | Lumber deriving (Eq, Show)
@@ -58,10 +59,10 @@ move bs ar ix = let
     Tree -> if length (filter (==Lumber) ns) >= 3 then Lumber else Tree
     Lumber -> if elem Lumber ns && elem Tree ns then Lumber else Open
 
--- readMA = MA.unsafeRead
--- writeMA = MA.unsafewrite
-readMA = MA.readArray
-writeMA = MA.writeArray
+readMA = MA.unsafeRead
+writeMA = MA.unsafeWrite
+-- readMA = MA.readArray
+-- writeMA = MA.writeArray
 
 doit2 :: Int -> G -> IO G --(A.Array Int Char)
 doit2 n seed = do
@@ -98,12 +99,28 @@ doit2 n seed = do
     fz <- go n old new >>= MA.freeze
     pure $ A.ixmap bnds flatten fz
 
-
+-- x, y, z .... [ LOOP ] [ LOOP ] ..... [LOOP, ..., 1000000000.
+findLoop :: [G] -> (Int, Int, G)
+findLoop = go mempty 0
+  where
+    go ss !n (g:gs)
+      | Just k <- M.lookup g ss = (n-k, n, g)
+      | otherwise = go (M.insert g n ss) (n+1) gs
 
 main = do
   i <- build . lines <$> readFile "input"
 --  traverse_ (putStrLn . pt) (take 11 (doit i))
   print (solve 10 (doit i))
+  let (period, endOfLoop, g) = findLoop (doit i)
+
+--  print lp
+  let n =  (1000000000 - endOfLoop) `mod` period
+
+  -- let scores = map score (doit i)
+  -- print $ take 1000 scores
+  print $ solve n (doit g)
+--  print n
+
 
   -- first10 <- traverse (\n -> doit2 n i) [0..1]
   -- putStrLn "================================================================================"
@@ -114,5 +131,6 @@ main = do
   -- traverse_ (putStrLn. pt) first10
   -- print $ "doit == doit2: " <> show (map pt (take 2 (doit i)) == map pt first10)
 
-  doit2 10 i >>= print . score
-  doit2 1000000000 i >>= print . score
+--  doit2 10 i >>= print . score
+
+--  doit2 1000000000 i >>= print . score
